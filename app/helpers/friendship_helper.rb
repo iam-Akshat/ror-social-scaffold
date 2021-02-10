c561ad492b4c30cebe2d27508a229e5a242920e2
Akshat Sethi <masterakshata@gmail.com>
2021-02-10 17:11:11 +0530

    add freindship status on users' index page

--- app/helpers/friendship_helper.rb
+++ app/helpers/friendship_helper.rb
@@ -1,15 +1,15 @@
 module FriendshipHelper
-    def current_status
-        return nil if current_user == @user 
-        friendship = Friendship.find_by(send_friend: current_user,recieved_friend: @user)
+    def current_status(user)
+        return nil if current_user == user
+        friendship = Friendship.find_by(send_friend: current_user,recieved_friend:user )
         if friendship.nil?
             false
         else
             friendship.status
         end
     end
-    def friend_or_not
-        case self.current_status
+    def friend_or_not(user)
+        case self.current_status(user)
         when 1
             'Request sent'
         when 2
